49e9be50a3af2851d7ecee385a979c13f1e1bd81
onjoku <49341640+onjoku@users.noreply.github.com>
2019-04-17 15:17:50 -0400

    Update console_2.sql

--- console_2.sql
+++ console_2.sql
@@ -1,3 +1,5 @@
+import sqlite3
+connect = sqlite3.connect('student.db')
 CREATE TABLE Instructor_Summary(
     '('CWID INTEGER primary key ,
     NAME  CHARACTER(20),
@@ -6,9 +8,14 @@ CREATE TABLE Instructor_Summary(
     STUDENT TEXT')'
 );
 
+print("Instructors Summary file")
 
+connect.execute('''CREATE TABLE INSTRUCTOR_SUMMARY
+            (CWID INT PRIMARY KEY   NOT NULL,
+            NAME              TEXT   NOT NULL,
+            DEPT               TEXT  NOT NULL,
+            COURSE             TEXT   NOT NULL,
+            STUDENT            INT    NOT NULL);''')
+print("Instructor's Summary Table was created successfully")
 
-
-
-
-
+connect.close()
